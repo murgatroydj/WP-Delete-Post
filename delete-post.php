f2979404bc8ddd63175b8de5bb315e86316a3187
James Murgatroyd <james@jamesmurgatroyd.com>
2017-07-18 12:02:39 -0600

    Add Comments and code cleanup

--- delete-post.php
+++ delete-post.php
@@ -1,59 +1,65 @@
 <?php 
   
 /*
-
-Plugin Name: Delete Specific Post
-Description: Rid the post forever! go to /wp-admin/index.php?delete_post=[post_id}
+Plugin Name: WP Delete Post
+Description: Remove a single wordpress post by going to /wp-admin/index.php?delete_post=[post_id}
 Version: 0.1
 Author: James Murgatroyd Communications
 License: GPL2
 */
-// set global scope vars
- 
+
+// set global scope vars 
 $the_id = $_GET['delete_post'];;
 $msg = ""; 
 $status = "notice-error";
 
-$the_id = $_GET['delete_post'];
-
+// Run Function on Admin init Hook
 add_action('admin_init', 'manually_delete_post', 0);
 
 function manually_delete_post(){
     
+    // Only run if user is logged in
     if(!is_user_logged_in())
         return;
-
+    
+    // Check user privledges
     if(!current_user_can('manage_options'))
         return;
-            
-     global $the_id;
-     
-     if($the_id){
           
-        //True force deletes the post and doesn't send it to the Trash
-        $delete = wp_delete_post( $the_id, true ); 
-        global $msg, $status;
+    // get global vars
+    global $the_id;
+    
+    if($the_id){
+          
+      //True force deletes the post and doesn't send it to the Trash
+      $delete = wp_delete_post( $the_id, true ); 
+      
+      // get global vars
+      global $msg, $status;
         
-        if($delete){
-          $msg = "Post $the_id deleted successfully!";
-          $status = "notice-success";
-        } else{
-          $msg = "Post $the_id was not deleted.";
-          $status = "notice-error";
-        }
+      if($delete){
+        $msg = "Post $the_id deleted successfully!";
+        $status = "notice-success";
+      } else{
+        $msg = "Post $the_id was not deleted.";
+        $status = "notice-error";
+      }
        
     }
 }
 
+
+// Add Admin Notice
 add_action( 'admin_notices', 'my_notice' );
 
+
 function my_notice() {
-   global $msg, $status;
+  // get global vars
+  global $msg, $status;
   ?>
   <div class="notice <?php echo $status; ?> is-dismissible">
       <p><?php echo $msg; ?></p>
   </div> 
   <?php 
   }
-
 ?>
